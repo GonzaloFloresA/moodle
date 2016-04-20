7515125b4dfc3618c28a95d3788f0812445fd1a9
GonzaloFloresA <gonzflores1969@gmail.com>
2016-04-20 16:50:47 -0400

    util BD

---
+++ utilBD.sql
@@ -0,0 +1,21 @@
+
+-- -----------------------------------------------------
+-- Table `mydb`.`mdl_key`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mdl_key` (
+  `key` INT NULL,
+  `llave` VARCHAR(45) NULL)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `mydb`.`mdl_sacar`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mdl_sacar` (
+  `id` INT NULL,
+  `email` VARCHAR(45) NULL,
+  `nombre` VARCHAR(45) NULL)
+ENGINE = InnoDB;
+
+
+
